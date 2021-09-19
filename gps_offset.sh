32ec402d783db44f06487adf9a8f6c834fc0c068
Shankar Kulumani <shanks.k@gmail.com>
2021-09-19 14:20:41 -0400

    update configurations

--- gps_offset.sh
+++ gps_offset.sh
@@ -5,10 +5,14 @@ TIMESPAN="60s"
 echo "Determine GPS Serial delay"
 # check if file exists and delete if so
 if [ -f ~/gps_offset.txt ]; then
-rm ~/gps_offset.txt
+    echo "Removing old log file"
+    rm ~/gps_offset.txt
 fi
-
+echo "Make sure ntp.conf GPS server has no time offset"
+echo "Recording GPS offset"
 timeout ${TIMESPAN} watch -n 0.5 "ntpq -pn | grep -E '.GPS. +0 +l +1 ' | tee --append ~/gps_offset.txt"
+
+echo "Finished. Computing average"
 
 # compute average
 awk '{total=total+$9; count=count+1} END {print "Total:"total; print "Count:"count; print "Avg:"total/count}' ~/gps_offset.txt
