509a71e1f261fc790c4fd4a0ef4492088c6c41ea
Shankar Kulumani <shanks.k@gmail.com>
2021-09-20 18:58:14 -0400

    Update mrtg config

---
+++ reload_config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Reloading SNMP, NTP, MRTG, and Apace"
+
+echo "Copying config files"
+sudo cp mrtg.cfg /etc/mrtg/mrtg.cfg
+sudo cp mrtg.conf /etc/apache2/sites-available/mrtg.conf
+
+echo "Restart MRTG"
+sudo indexmaker /etc/mrtg.cfg > /home/pi/www/mrtg/index.html
+sudo /etc/init.d/mrtg restart
+
+sudo a2ensite mrtg
+
+echo "Restart apache2"
+sudo systemctl restart apache2
+
+echo "Restart NTP"
+sudo systemctl restart ntp
+
+echo "Restart SNMP"
+sudo systemctl restart snmp
+
