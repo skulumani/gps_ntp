0ecc04090e7e9e24ed32adc8a5b1d5ce10e9b23e
Shankar Kulumani <shanks.k@gmail.com>
2021-09-20 21:28:10 -0400

    Update reload script and disable ntp logging

--- reload_config.sh
+++ reload_config.sh
@@ -6,6 +6,10 @@ echo "Copying config files"
 sudo cp -a ./mrtg/. /etc/mrtg/
 sudo cp mrtg.conf /etc/apache2/sites-available/mrtg.conf
 
+sudo cp dhclient.conf /etc/dhcp/dhclient.conf
+sudo cp ntp.conf /etc/ntp.conf
+sudo cp snmpd.conf /etc/snmp/snmpd.conf
+
 echo "Restart MRTG"
 sudo indexmaker --section=title /etc/mrtg/mrtg.cfg > /home/pi/www/mrtg/index.html
 sudo /etc/init.d/mrtg restart
