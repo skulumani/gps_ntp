0d1818054296fd24b33d329cf0ac1e66699c7ae4
Shankar Kulumani <shanks.k@gmail.com>
2021-09-22 09:26:55 -0400

    Start on Rpi RRD

--- rrd.py
+++ rrd.py
@@ -2,22 +2,37 @@
 
 # create the required RRD databases
 import rrdtool
+import sys
+import os
 
 # RPi data
 def create_rrd():
-  rrdtool.create()
+    main_path = "/home/pi/rrd"
+    if not os.path.isdir(main_path):
+        os.makedires(main_path)
+      
+  # RPi data
+  rrdtool.create(
+      os.path.join(main_path, "rpi_data.rrd"),
+      "--start", "now", 
+      "--step", "10", 
+      "DS:cpu_usage_percent:GAUGE:20:0:100",
+      "DS:mem_usage_percent:GAUGE:20:0:100",
+      "DS:disk_usage_percent:GAUGE:20:0:100",
+      "DS:cpu_temp:GAUGE:20:-20:100"
+     )
 
 def add_rpi_data():
-  pass
+    pass
 
 def add_gps_stats():
-  pass
+    pass
 
 def add_gps_sats():
-  pass
+    pass
 
 def add_ntp_data():
-  pass
+    pass
 
 # GPS data
 
