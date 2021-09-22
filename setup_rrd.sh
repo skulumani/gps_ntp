d7428299be50b8c26dbec36a24fa4bf3762153ea
Shankar Kulumani <shanks.k@gmail.com>
2021-09-21 22:36:35 -0400

    Update rrd setup tool

--- setup_rrd.sh
+++ setup_rrd.sh
@@ -2,8 +2,8 @@
 
 echo "Install rrdtool and python3 bindings"
 
-sudo apt-get install librrd-dev rrdtool libpython-dev
-sudo apt-get install python3 python3-pip
+sudo apt-get install librrd-dev rrdtool -y
+sudo apt-get install python3-pip -y
 
 pip3 install rrdtool
 pip3 install matplotlib
