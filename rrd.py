d5087341c35a4a179fe986854b840d475fe304d2
Shankar Kulumani <shanks.k@gmail.com>
2021-09-22 08:57:50 -0400

    Update and rename create_rrd.py to rrd.py

---
+++ rrd.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# create the required RRD databases
+import rrdtool
+
+# RPi data
+def create_rrd():
+  rrdtool.create()
+
+def add_rpi_data():
+  pass
+
+def add_gps_stats():
+  pass
+
+def add_gps_sats():
+  pass
+
+def add_ntp_data():
+  pass
+
+# GPS data
+
+# GPS satellites
+
+# NTP data
