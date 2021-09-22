99413258156353f9ce412942f7d2b687212fcb9d
Shankar Kulumani <shanks.k@gmail.com>
2021-09-21 22:08:13 -0400

    Add rrd setup script

---
+++ create_rrd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# create the required RRD databases
+import rrdtool
+
+# RPi data
+rrdtool.create(
+
+# GPS data
+
+# GPS satellites
+
+# NTP data
--- setup_rrd.sh
+++ setup_rrd.sh
@@ -2,5 +2,9 @@
 
 echo "Install rrdtool and python3 bindings"
 
-sudo apt-get install python3-rrdtool rrdtool
+sudo apt-get install librrd-dev rrdtool libpython-dev
+sudo apt-get install python3 python3-pip
+
+pip3 install rrdtool
+pip3 install matplotlib
 
