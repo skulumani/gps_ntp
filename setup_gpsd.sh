8e73276ae7311523bebf02d6b35c8165ed77da21
Shankar Kulumani <shanks.k@gmail.com>
2021-10-03 13:50:40 -0400

    Add GPSD build script

---
+++ setup_gpsd.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Update GPSD"
+
+GPS_VERSION="3.23.1"
+WORK_DIR=$(mktemp -d)
+
+sudo apt-get update
+sudo apt-get install -y scons libncurses-dev python-dev pps-tools git-core asciidoctor python3-matplotlib build-essential manpages-dev pkg-config
+
+# download GPSD
+wget http://download.savannah.gnu.org/releases/gpsd/gpsd-${GPS_VERSION}.tar.gz -O ${WORK_DIR}/gpsd.tar.gz
+cd $WORK_DIR
+
+tar -xzf gpsd.tar.gz -C gpsd
+cd gpsd
+
+sudo scons
+# sudo scons --config=force
+
+sudo scons install
+
+echo "Hold GPSD with apt"
+echo "gpsd hold" | sudo dpkg --set-selections
+gpsd -V
+
+
+
